9e0545f4c11108d0bc9fc70a877f7a66383b5b53
Arnaud Volpi <arnaudvolp@gmail.com>
2022-07-14 16:07:45 +0200

    new design part 1

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -14,21 +14,21 @@ export class PostsComponent implements OnInit {
 
   posts = [
     {
-      img: 'https://images.pexels.com/photos/1376201/pexels-photo-1376201.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      img: 'https://images.pexels.com/photos/1940038/pexels-photo-1940038.jpeg?auto=compress&cs=tinysrgb&w=1600',
       date: '01 Septembre 2022',
-      title: 'Meildo - Lancement officiel !',
+      title: 'Layxo - Lancement officiel !',
       text: "Après six mois de travail dans l'ombre, c'est le grand jour Polydev se lance officiellement sur le marché du travail !"
     },
     {
-      img: 'https://images.pexels.com/photos/1534172/pexels-photo-1534172.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      img: 'https://images.pexels.com/photos/2051027/pexels-photo-2051027.jpeg?auto=compress&cs=tinysrgb&w=1600',
       date: '16 Août 2022',
-      title: 'Meildo - Derniers préparatifs',
+      title: 'Layxo - Derniers préparatifs',
       text: "Dernière ligne droite avant le lancement officiel, toute l'équipe met les bouchées doubles pour préparer le lancement ...."
     },
     {
-      img: 'https://images.pexels.com/photos/1687341/pexels-photo-1687341.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      img: 'https://images.pexels.com/photos/1940041/pexels-photo-1940041.jpeg?auto=compress&cs=tinysrgb&w=1600',
       date: '15 Mai 2022',
-      title: 'Meildo - Création de notre entreprise',
+      title: 'Layxo - Création de notre entreprise',
       text: "Aujourd'hui est un grand jour, nous vous présentons Meildo notre projet entrepreneurial."
     }
   ]
